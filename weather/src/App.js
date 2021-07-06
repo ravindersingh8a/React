25fb9ec2108ded9ba97d8fd5a9fc16ebe5f7e7b6
Ravinder <ravindersingh8a>
2021-07-06 09:07:26 +0530

    weather App

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React,{useState} from 'react';
 
 function App() {
+  
+  const [city,setCity] = useState("");
+  const [result,setResult] = useState("");
+
+  const changeHandler = e =>{
+    setCity(e.target.value);
+    
+  }
+  const submitHandler = e =>{
+    e.preventDefault();
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d885aa1d783fd13a55050afeef620fcb`).then(
+      response=> response.json()).then(
+        data => {
+          const kelvin = data.main.temp;
+          const celcius = kelvin - 273.15;
+          setResult("Temperature at "+city+"\n"+Math.round(celcius)+"°C");
+        }
+      ).catch(error => console.log(error))
+      setCity("");
+    console.log(city)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+        <div className="card">
+          <div className="card-body">
+            <h1 className="card-title">Weather App</h1>
+            <form onSubmit={submitHandler}>
+              <input type="text" name="city" value={city} onChange={changeHandler}/>
+              <br/>
+              <br/>
+
+              <input type="submit" value="Get Temperature"/>
+            </form>
+            <br></br>
+            <div>
+               <h1>{result}</h1> 
+            </div>
+
+          </div>
+
+        </div>
+      </center>
     </div>
   );
 }
